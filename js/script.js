0e1392265825b5a37ed4aedcf3e1d7231e48ab53
Alex Kim <kim4a@cmich.edu>
2017-11-27 20:24:28 -0500

    high fives!

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-// create mailto link
-var em = document.getElementById('em');
-if (em != null) {
-	var temp = "kim4a&#064;cmich&#046;edu";
-	em.innerHTML = "<a href=\"mailto:" + temp + "\">" + temp + "</a>";
+window.onload = function(e) {
+	// Prevent double-tap zoom on mobile
+	document.body.addEventListener('touchstart', preventZoom);
+	// create mailto link
+	var em = document.getElementById('em');
+	if (em != null) {
+		var temp = "kim4a&#064;cmich&#046;edu";
+		em.innerHTML = "<a href=\"mailto:" + temp + "\">" + temp + "</a>";
+	}
+	// parse twemoji
+	twemoji.parse(document.body, {size: 1, folder: 'svg', ext: '.svg'});
+	// High five
+	document.getElementById('hand').addEventListener("touchstart", hi5);
+	document.getElementById('hand').addEventListener("click", hi5);
 }
-
-// parse twemoji
-twemoji.parse(document.body, {size: 1, folder: 'svg', ext: '.svg'});
+function preventZoom(e) {
+	var t2 = e.timeStamp;
+	var t1 = e.currentTarget.dataset.lastTouch || t2;
+	var dt = t2 - t1;
+	var fingers = e.touches.length;
+	e.currentTarget.dataset.lastTouch = t2;
+	if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+	e.preventDefault();
+	e.target.click();
+}
+function hi5() {
+	var hand = document.getElementById('hand');
+	hand.classList.toggle('spin');
+}
